832af070372a654b9f6029480061fed1ad6e695b
Stefano Mazzocchi <smazzocchi@singularity6.com>
2023-05-11 18:48:45 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dag
+
+# How to install
+
+Run `pip install -r requirements.txt` to install
+
+# How to run
+
+Run './dag.py' to run dag and then point your browser to http://127.0.0.1:8080/
---
+++ dag.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""Poor man task dependency maps as Sankey diagrams"""
+
+from pathlib import Path
+
+import bottle
+
+HOST = '0.0.0.0'
+PORT = '8080'
+WEB_FOLDER = "web"
+
+def main():
+
+    dest = (Path(__file__).parent / WEB_FOLDER).resolve()
+    print(f"Serving files from {dest}")
+
+    @bottle.route('/')
+    def serve_index():
+        return bottle.static_file('index.html', root=dest)
+
+    bottle.run(host=HOST, port=PORT)
+
+if __name__ == '__main__':
+    main()
